87256c60a938edca8bb981508ccb019335204140
Ali Amin <prof.aliamin@gmail.com>
2023-10-24 23:50:13 +0300

    fixed the responsiveness of the project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -7,26 +7,20 @@
    Last modification: 03/06/2023
 */
 
-import SectionTitle from "../Components/SectionTitle";
 import ProjectCards from "../Components/ProjectCards";
+import SectionTitle from "../Components/SectionTitle";
 
 const Projects = () => {
-  return (
-    <div id="projects" className="w-full overflow-hidden-web flex justify-center">
-      <div className="w-full min-h-[800px] h-screen xl:w-[70%] relative mt-40 flex flex-col items-center justify-center pb-36">
-        <div className="w-full h-[180%] mt-[-10%] mb-[-10%]">
-          <SectionTitle title="PROJECTS" subtitle="What I have done so far" />
-        </div>
-        <div className="w-full h-[70%] z-10 flex justify-center">
-          <div className="w-full xl:w-[70%] h-[-10%">
-            <ProjectCards />
-          </div>
-          <div className="absolute w-full h-full top-[100%] right-[-75%] sm:right-[-50%]">
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full flex justify-center ">
+			<div className="w-full xl:w-[70%] flex flex-col pb-16">
+				<div className="w-full ">
+					<SectionTitle title="PROJECTS" subtitle="What I have done so far" />
+				</div>
+				<ProjectCards />
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
