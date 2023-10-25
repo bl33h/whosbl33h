8ca7b5878559c410193b13c81a37dc7324b10c6f
Ali Amin <prof.aliamin@gmail.com>
2023-10-25 09:27:55 +0300

    updated navbar for better visibility

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -12,7 +12,7 @@ import SectionTitle from "../Components/SectionTitle";
 
 const Projects = () => {
 	return (
-		<div className="w-full flex justify-center ">
+		<div id="projects" className="w-full flex justify-center ">
 			<div className="w-full xl:w-[70%] flex flex-col pb-16">
 				<div className="w-full ">
 					<SectionTitle title="PROJECTS" subtitle="What I have done so far" />
