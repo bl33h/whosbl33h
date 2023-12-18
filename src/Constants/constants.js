1a4caaf713df224cac4ba84663a808052473c750
bl33h <per21371@uvg.edu.gt>
2023-12-18 00:07:42 -0600

    bugs fixed

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -83,12 +83,7 @@ const introduction = {
   text: [
     "Hello there, I'm so happy you are here! (:",
 
-<<<<<<< HEAD
 		"My name is Sara, I'm 21 and a junior computer science student based in Guatemala. I have a strong passion for the technologies related to the data base construction and UX.",
-=======
-    "My name is Sara, I'm 20 and a third year computer science student based in Guatemala. I have a strong passion for the technologies related to the data base construction and UX.",
->>>>>>> 173e06a51e36fdd47f91927259cc2a87461dbfda
-
     "Outside of my regular activities, I'm into drawing whenever I have some free time. I'm also a huge dog lover and enjoy playing chess for fun.",
 
     "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
