2a4125d768ad9970d796a54095f03d9c987a718f
bl33h <per21371@uvg.edu.gt>
2024-04-20 01:24:49 -0600

    particles changed

--- src/Components/Particles/particlesOptions.js
+++ src/Components/Particles/particlesOptions.js
@@ -117,7 +117,7 @@ export const heroOptions = {
       mode: 'bounce',
     },
     color: {
-      value: '#fff',
+      value: '#E1A2C1',
     },
     move: {
       angle: {
@@ -168,28 +168,21 @@ export const heroOptions = {
           fill: true,
           font: 'Font Awesome 6 Brands',
           style: '',
-          value: ['\uf13b'],
+          value: ['\uf3c6'],
           weight: '400',
         },
         {
           fill: true,
           font: 'Font Awesome 6 Brands',
           style: '',
-          value: ['\uf38b'],
+          value: ['\uf17c'],
           weight: '400',
         },
         {
           fill: true,
           font: 'Font Awesome 6 Brands',
           style: '',
-          value: ['\uf3b9'],
-          weight: '400',
-        },
-        {
-          fill: true,
-          font: 'Font Awesome 6 Brands',
-          style: '',
-          value: ['\uf41b'],
+          value: ['\uf791'],
           weight: '400',
         },
       ],
