46229b7cd72a0f0fde513a822c3f9147f635c6cb
bl33h <per21371@uvg.edu.gt>
2024-04-20 02:26:17 -0600

    projects navbar bug fixed

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -7,17 +7,17 @@
    Last modification: 03/06/2023
 */
 
-import ProjectCards from "../Components/ProjectCards";
+import Works from "../Components/ProjectCards";
 import SectionTitle from "../Components/SectionTitle";
 
 const Projects = () => {
 	return (
-		<div className="w-full flex justify-center ">
+		<div className="w-full flex justify-center" id="projects">
 			<div className="w-full xl:w-[70%] flex flex-col pb-16">
 				<div className="w-full ">
 					<SectionTitle title="PROJECTS" subtitle="What I have done so far" />
 				</div>
-				<ProjectCards />
+				<Works/>
 			</div>
 		</div>
 	);
