0ef6119539fe82677e5f265fb01e9225d5469180
bl33h <per21371@uvg.edu.gt>
2024-04-20 03:12:35 -0600

    constant bugs fixed

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -84,7 +84,7 @@ const introduction = {
     "Hello there, I'm so happy you are here! (:",
 
 		"My name is Sara, I'm 21 and a junior computer science student based in Guatemala. I have a strong passion for the technologies related to QA and AI.",
-    "Outside of my regular activities, I'm a painting artist, football enthusiast, chess player, dog lover and computer science student.",
+    "Outside of my regular activities, I'm a painting artist, football enthusiast, chess player,and a dog lover.",
     "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
     "You can download my CV here."
   ],
