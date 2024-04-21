f6a3ba2037d3faa370a3461e1756817d5ff11b5e
bl33h <per21371@uvg.edu.gt>
2024-04-20 20:56:12 -0600

    bold navbar titles

--- src/Components/nav/NavBar.jsx
+++ src/Components/nav/NavBar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                 key={link.id}
                 className={`${
                   active === link.title ? "text-white" : "text-secondary"
-                } hover:text-white text-[18px] font-medium cursor-pointer `}
+                } hover:text-white text-[24px] font-bold cursor-pointer `}
                 onClick={() => setActive(link.title)}
               >
                 <a href={`#${link.id}`}>{link.title}</a>
