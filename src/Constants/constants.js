0cca06276f32597f334baa6b2f040fc265a12e75
bl33h <per21371@uvg.edu.gt>
2024-04-21 00:08:10 -0600

    added all the projects for a nice view

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -159,6 +159,13 @@ const projects = [
     demo_link: 'https://camo.githubusercontent.com/59b3c9969f369559fbeb1ed6a53f159e0e83b1151b82552b5b2165c4e9686e89/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f312f31612f4669626f6e616363695f63616c6c5f747265655f352e676966',
   },
   {
+    name: 'Visit Our Lands',
+    description: 'It is a recommendation system built with React and Supabase. This system focuses on providing tourist recommendations for various locations in Guatemala.',
+    image: "https://res.cloudinary.com/worldpackers/image/upload/c_fill,f_auto,q_auto,w_1024/v1/guides/article_cover/uvd82gspopw8orvryulc",
+    source_code_link: 'https://github.com/bl33h/visitOurLands',
+    demo_link: 'https://visitourlands.netlify.app/',
+  },
+  {
     name: 'Client-Server Chat',
     description: 'This is a chat application that facilitates communication between multiple clients through a centralized server. The application is designed using socket programming and handles multiple client connections concurrently using threads.',
     image: "https://i.pinimg.com/originals/0e/4a/c3/0e4ac37acbff81cd087aa19692a07a9d.gif",
