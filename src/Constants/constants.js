638b2fa5a0a319a6173cf7c5e74f4e6701eca679
bl33h <per21371@uvg.edu.gt>
2024-04-27 12:34:01 -0600

    bugs fixed

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -85,7 +85,7 @@ const introduction = {
 		"My name is Sara, I'm 21 and a junior computer science student based in Guatemala. I have a strong passion for the technologies related to QA and AI.",
     "Outside of my regular activities, I'm a painting artist, football enthusiast, chess player, and a dog lover.",
     "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
-    "You can download my CV here."
+    "You can download my resume here."
   ],
 };
 
