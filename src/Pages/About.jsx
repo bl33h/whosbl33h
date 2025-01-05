47250a9ebf867acc8d3f2d4fa0565f16f663b158
bl33h <per21371@uvg.edu.gt>
2025-01-05 16:46:06 -0600

    feat: libs latest version

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -39,13 +39,6 @@ const About = () => {
                 <span className="text-primary-500" style={{ fontSize: '0.95em', fontWeight: '500'}}>{introduction.text[1]}</span>
                 <span className="text-primary-500" style={{ fontSize: '0.95em', fontWeight: '500'}}>{introduction.text[2]}</span>
                 <span className="text-primary-500" style={{ fontSize: '0.95em', fontWeight: '500'}}>{introduction.text[3]}</span>
-                <a href="cv/SaraEcheverriasCV2024.pdf" download>
-                  <span className="text-primary-600" style={{ fontSize: '0.95em', fontWeight: '700'}}>
-                    {/* {introduction.text[4].split('here')[0]}
-                    <span style={{ textDecoration: 'underline' }}>here</span>
-                    {introduction.text[4].split('here')[1]} */}
-                  </span>
-                </a>
               </m.p>
             </LazyMotion>
           </div>
